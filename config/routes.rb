784625d5b1e07a1a9479938a37ab8c64acc98b89
bernardo mendes <bmmendes2001@icloud.com>
2023-02-22 18:01:04 +0000

    unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :flats, only: [ :index, :new, :show, :create, :edit, :destroy, :update ]
+
 end
